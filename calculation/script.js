0a74e60bb83a9b7091257dc2bc2ee2ede20b4c6c
BAHODIR <st.bahodir@gmail.com>
2023-01-03 21:12:32 +0500

    calculation/математический вычислений

---
+++ calculation/script.js
@@ -0,0 +1,28 @@
+let x = 10 + 5; //15
+let y = 10 - 5; //5
+let z = 10 * 5; //50
+let q = 10 / 5; //2
+let w = 10 % 3; //1
+
+let complex = 10 + 5 * 10 - 8 / 2; //56
+let complex_2 = (10 + 5) * (10 - 8) / 2; //15
+
+let a = 10;
+a++; //11
+a--; //10
+++a; //11
+--a; //10
+
+console.log(a++); //10
+console.log(a); //11
+console.log(a--); //11
+console.log(a); //10
+
+
+// короткая / short
+//let x = 10 + 5; //15
+x += 5; // x = x + 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
